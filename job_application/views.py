60c4be9832e24a12a2d00655a00460f69c2fe1c7
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-16 13:36:06 +0200

    Storing of data

--- job_application/views.py
+++ job_application/views.py
@@ -11,6 +11,9 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
             
 
     return render(request, "index.html")
